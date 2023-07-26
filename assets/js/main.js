9db30944b475ddf502b7c359e9fc70f9bd7c0a92
Andrew Baxter <andrew@toughbaxter.com>
2023-07-26 15:50:29 -0700

    Various QA styling & functionality fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+import './utilities/slideToggle.js';
 import { initializeAccordions } from './blocks/accordions.js';
 import { initializeTeamMembers } from './blocks/teamMember.js';
 import { initializeTimelines } from './blocks/timeline.js';
@@ -5,8 +6,7 @@ import { initializeGoals } from './blocks/goals.js';
 import { initializeSubMenu } from './components/menu.js';
 import { initializePostSliders } from './blocks/postSlider.js';
 import { initializeFAQs } from './blocks/faqs.js';
-
-/* global DataTable */
+import { initializePole } from './blocks/pole.js';
 
 // Call the initializeAccordions function to initialize the accordions
 initializeAccordions();
@@ -16,7 +16,4 @@ initializeTimelines();
 initializeGoals();
 initializePostSliders();
 initializeFAQs();
-
-document.addEventListener( 'DOMContentLoaded', function() {
-	new DataTable( '.wp-block-table table' );
-} );
+initializePole();
